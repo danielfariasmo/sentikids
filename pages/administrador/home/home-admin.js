939fd002ee4796328f43e0a5110d36fd90150306
danielfariasmo <fariasd99@gmail.com>
2025-02-15 14:51:07 +0100

    Inicio-Adminitrador 100% corregida

--- pages/administrador/home/home-admin.js
+++ pages/administrador/home/home-admin.js
@@ -1,5 +1,6 @@
-// Utilidad de menu "hamburguesa"
-
+/*---------------------------------------------------------------
+Utilidad de menu "hamburguesa"
+---------------------------------------------------------------*/
 function toggleMenu() {
     let menuList = document.querySelector('.menu ul')
     menuList.classList.toggle('active')
