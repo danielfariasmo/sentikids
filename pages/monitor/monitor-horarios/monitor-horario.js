e3feae8dc97626339da5d8fd53364c7d288be2c5
DanielGonzalezGarrote_ExamenGit <158164494+daaaaniel242@users.noreply.github.com>
2025-02-18 10:54:12 +0100

    Cambio

--- pages/monitor/monitor-horarios/monitor-horario.js
+++ pages/monitor/monitor-horarios/monitor-horario.js
@@ -1,4 +1,29 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuContainer = document.querySelector(".menu-container");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+
+  menuContainer.addEventListener("mouseenter", function () {
+      dropdownMenu.style.display = "block";
+  });
+
+  menuContainer.addEventListener("mouseleave", function () {
+      dropdownMenu.style.display = "none";
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const logoutBtn = document.getElementById("logoutBtn");
+
+  logoutBtn.addEventListener("click", function () {
+      window.location.href = "../../web/home/inicio.html"; 
+  });
+});
+
+
+
+
 // Función para cargar el horario
+
 function cargarHorario() {
     fetch('monitor-horario.php')
       .then(response => response.json())
