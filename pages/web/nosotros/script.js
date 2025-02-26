a7176a3cd8c2abaaf89528343e3f70cf88b35bd8
DanielGonzalezGarrote_ExamenGit <158164494+daaaaniel242@users.noreply.github.com>
2025-02-26 09:44:53 +0100

    Menu responsive

--- pages/web/nosotros/script.js
+++ pages/web/nosotros/script.js
@@ -25,24 +25,33 @@ var swiper = new Swiper(".slide-content", {
         },
     },
 });
-//MENÚ
+// MENÚ HAMBURGUESA
+const hamburguesa = document.querySelector(".hamburguesa");
+const menuMovil = document.querySelector(".menu-movil");
+
+hamburguesa.addEventListener("click", () => {
+    menuMovil.classList.toggle("visible");
+});
+
+// Cerrar menú al hacer clic en un enlace
+const enlacesMovil = document.querySelectorAll(".opciones-movil a");
+enlacesMovil.forEach(enlace => {
+    enlace.addEventListener("click", () => {
+        menuMovil.classList.remove("visible");
+    });
+});
+
+// MENÚ FIJO (tu código existente)
 const menuFijo = document.querySelector(".menu-fijo");
 menuFijo.style.marginTop = "48px";
 const MARGEN_INICIAL = 48;
 
 window.addEventListener("scroll", function () {
     const scrollActual = window.scrollY;
-
-    // Calcula el margen superior basado en el scroll
     let margenSuperior = MARGEN_INICIAL - scrollActual;
-
-    // Asegura que el margen no sea menor que 0
     margenSuperior = Math.max(margenSuperior, 0);
-
-    // Aplica el margen calculado
     menuFijo.style.marginTop = `${margenSuperior}px`;
 });
-
 //EMPRESAS COLABORADORAS
 document.addEventListener("DOMContentLoaded", function () {
     var swiperEmpresas = new Swiper(".empresas-swiper", {
